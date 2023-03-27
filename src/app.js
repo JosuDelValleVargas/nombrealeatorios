0ab6cce27aa10d36f7365cd1146ca744fd267074
Josu DelValleVargas <josudelvallevargas@gmail.com>
2023-03-27 19:23:18 +0000

    entrega final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
 };
